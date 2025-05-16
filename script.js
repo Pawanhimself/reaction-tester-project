0316b3a1aaa17e66dc47293b90c3b1d62db12250
Pawan Kumar <113756891+Pawanhimself@users.noreply.github.com>
2025-05-16 08:32:56 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,35 @@
+var startTime = new Date().getTime();
+
+// when user click on this div then this event is called and 
+// run the script inside this evenet
+document.getElementById('shape').onclick = function(){
+
+    divPosition();
+    let endTime = new Date();
+    let timeTaken = ( endTime - startTime ) / 1000;
+    alert(timeTaken);
+}
+
+//calling div position so when the page is loaded for the first time it style the div
+divPosition();
+
+//divPosition function is used to place div to random position after the use click on it
+function divPosition(){
+   
+    let left = (Math.random() * 30);
+    let top = (Math.random() * 30);
+    let wh = (Math.random() * 10) + 20;
+
+    let divElem = document.getElementById('shape');
+    divElem.style.left = `${left}rem`;
+    divElem.style.top = `${top}rem`;
+    divElem.style.width = `${wh}rem`;
+    divElem.style.height = `${wh}rem`;
+    divElem.style.backgroundColor = getRandomColorHex();
+}
+
+//this function return the random hex color code to change the div color every time when the user click on it
+function getRandomColorHex() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+  
